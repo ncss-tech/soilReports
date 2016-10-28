f1f3a107cc5ecde9239fa434f7295c78c9d7b737
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-10-28 13:46:39 -0700

    added basic output for spatial stats... not used for anything yet

--- inst/reports/region2/mu-comparison/config.R
+++ inst/reports/region2/mu-comparison/config.R
@@ -70,7 +70,7 @@ mu.col <- 'MUSYM'
 
 # values less < 1 (coarse sampling density) will result in variation between runs, and un-sampled polygons
 # values > 10 will result in longer report run times
-pts.per.acre <- 2
+pts.per.acre <- 0.5
 
 
 
