6f3e43f6f92f1e94f931de60bcbd2a5957f8decd
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-10-28 11:29:07 -0700

    adding support for calculation of effective N, still very slow

--- inst/reports/region2/mu-comparison/config.R
+++ inst/reports/region2/mu-comparison/config.R
@@ -80,9 +80,14 @@ pts.per.acre <- 2
 p.quantiles <- c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95)
 
 
+########################################################
+### correct sample size for spatial autocorrelation? ###
+########################################################
+correct.sample.size <- TRUE
+
+
 ###########################################
 ### save samples after report has run ? ###
 ###########################################
-cache.samples <- FALSE
-
+cache.samples <- TRUE
 
