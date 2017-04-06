1e64c3fc0e16c66b064d3350eb35ec5ae0a6b631
Stephen Roecker <stephen.roecker@gmail.com>
2017-04-06 10:26:09 -0400

    adding Generic_rules.R files for Region 11

---
+++ inst/reports/region11/lab_summary_by_taxonname/genhz_rules/Generic_rules.R
@@ -0,0 +1,23 @@
+ghr <- list(
+  n = c("Ap",
+        "BA",
+        "E", 
+        "Bt",
+        "Btg",
+        "2Bt", 
+        "2BC", 
+        "C",
+        "Cd",
+        "missing"),
+  
+  p = c("A",
+        "BA|A3|B1",
+        "^E|A2",
+        "t|T",
+        "g",
+        "2B|IIB",
+        "BC|B3",
+        "C",
+        "d",
+        "missing")
+  )
