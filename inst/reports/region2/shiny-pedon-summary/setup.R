7745a308e976006d04121d7b06c08212b7455588
Brown <12001002844681@FEDIDCARD.GOV>
2017-04-26 14:17:53 -0700

    added stable version of Shiny interactive pedon summary / component report; still does not conform to default soilReportsetup (i.e. needs both shiny.Rmd and report.Rmd); may be worth creating a separate tier for Shiny-interfaces that create static reports?

---
+++ inst/reports/region2/shiny-pedon-summary/setup.R
@@ -0,0 +1,13 @@
+##
+## setup file for report, be sure to use standardized variable names
+##
+
+
+## note: this will not update installed packages... could lead to dependency-related errors
+## packages + deps from CRAN
+.packages.to.get <- c('soilDB','sharpshootR','rgdal','raster','leaflet','mapview','latticeExtra','rmarkdown','plyr','reshape2','xtable','knitr','shiny')
+
+
+## packages from GH, no deps
+.gh.packages.to.get <- c('ncss-tech/soilDB','ncss-tech/sharpshootR')
+
