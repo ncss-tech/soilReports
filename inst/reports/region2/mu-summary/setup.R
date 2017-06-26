bb121fb6a55ede6ac5d2e4adf6a30c2a930f9f46
Brown <12001002844681@FEDIDCARD.GOV>
2017-06-26 14:11:59 -0700

    region2/mu-summary updated for use with report manifest via setup.R

--- inst/reports/region2/mu-summary/setup.R
+++ inst/reports/region2/mu-summary/setup.R
@@ -6,6 +6,19 @@
 packageurl <- "http://cran.r-project.org/src/contrib/Archive/knitr/knitr_1.12.tar.gz"
 install.packages(packageurl, repos=NULL, type="source")
 
+## report name
+.report.name <- 'mu-summary'
+
+## version number
+.report.version <- 1.0
+
+## short description
+.report.description <- 'summarize raster data for a large collection of map unit polygons'
+
+.files.to.copy <- c('report.Rmd','generate-samples.R','make-reports.R','config.R','README.md')
+.update.files.to.copy <- c('report.Rmd','generate-samples.R','make-reports.R','README.md')
+.has.shiny.interface <- FALSE
+
 # packages + deps from CRAN
 packages.to.get <- c('rmarkdown', 'rgdal', 'raster', 'plyr', 'reshape2', 'aqp', 'soilDB', 'sharpshootR', 'latticeExtra', 'clhs', 'devtools')
 res <- sapply(packages.to.get, install.packages, dep=TRUE)
