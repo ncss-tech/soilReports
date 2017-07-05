0bc4f65eaf1126eb07646fe86b86bdb4c3766dd4
Andrew Brown <andrew.g.brown@ca.usda.gov>
2017-07-05 15:39:06 -0700

    added two new functions that can be used by reportInit() for providing metadata access within report.
    using the R code chunk route is a direct drop in replacement for the old metadata handling. no changes to RO2 reports required.
    
    using the YAML will require all references to metadata within report to use the read-only "params" list. easy enough. although I was having issues getting this working correctly whilst knitting from RStudio, there's nothing wrong with the YAML itself as far as I can tell.

--- R/reportInit.R
+++ R/reportInit.R
@@ -5,7 +5,6 @@ reportInit <- function(reportName, outputDir=NULL, overwrite=FALSE, updateReport
   
   # get base directory where reports are stored within package
   base.dir <- system.file(paste0('reports/', reportName), package='soilReports')
-  print(base.dir)
   # all reports must have setup.R file
   setup.file <- paste0(base.dir, '/', 'setup.R')
   
@@ -27,6 +26,7 @@ reportInit <- function(reportName, outputDir=NULL, overwrite=FALSE, updateReport
     if(exists('.paths.to.copy', envir = env)) { #copy everything if not in "update" mode
       pa <- get('.paths.to.copy', envir = env)
       lapply(pa, FUN=copyPath, base.dir, outputDir)
+      print("Successfully initiated report!")
     } else stop("Failed to instantiate report -- no paths to copy specified in setup.R!")
   } else {
     #TODO: could try and infer which files are "config" and ignore those, requiring only the .paths.to.copy variable
@@ -34,6 +34,7 @@ reportInit <- function(reportName, outputDir=NULL, overwrite=FALSE, updateReport
     if(exists('.update.paths.to.copy', envir = env)) {
       pa <- get('.update.paths.to.copy', envir = env)
       lapply(pa, FUN=copyPath, base.dir, outputDir, overwrite=T)
+      print("Successfully updated report!")
       #TODO: should there be a check that required components are present? check against ".paths.to.copy"? only look for R/Rmds?
     } else stop("Failed to update report -- no update paths to copy specified in setup.R!")
   }
@@ -61,14 +62,14 @@ appendToTopOfFile <- function(filepath, what) {
   } else return(FALSE)
 }
 
-copyPath <- function(fname, srcDir, outputDir, overwrite = F) {
+copyPath <- function(fname, srcDir, outputDir, overwrite = F) { 
   src <- paste0(srcDir, '/', fname)
   dst <- paste0(outputDir, '/', fname)
   if(!dir.exists(src)) { #files will return false 
     if(file.exists(src)) {#but need to make sure it actually is a file
       #OK we have a path to a file
       if(overwrite | !file.exists(dst)) #but need to make sure that we can overwrite... or that it is not already extant
-         file.copy(from=src, to=outputDir, overwrite = overwrite)
+        file.copy(from=src, to=outputDir, overwrite = overwrite)
     } else {
       #we have a path to a directory
       if(!dir.exists(dst)) {
@@ -81,6 +82,47 @@ copyPath <- function(fname, srcDir, outputDir, overwrite = F) {
       }
     }
   }
+}
+
+defineInCodeChunk <- function(filepath, param.name, param.value) {
+  #NOTE: param values will be directly injected; need to include e.g. escaped quotes for strings
+  if(file.exists(filepath)) {
+    buf = c("```{r, echo=FALSE, results='hide', warning=FALSE, message=FALSE}")
+    for(p in 1:length(param.name)) buf=c(buf, paste0("\t",param.name[p], " <- ",param.value[p]))
+    buf = c(buf, "```")
+    fcon <- file(filepath, 'r+')
+    l <- readLines(fcon)
+    yaml_block <- grepl(l,pattern="^---$")
+    idx = 2
+    if(any(yaml_block)) {
+      idx = max(which(yaml_block))
+    }
+    l <- c(l[1:idx],buf,l[idx+1:length(l)]) #add below YAML but above everything else
+    writeLines(l,fcon)
+    close(fcon)
+    return(TRUE)
+  } else return(FALSE)
+}
+
+defineInYAMLHeader <- function(filepath, param.name, param.value) {
+  #NOTE: should be able to parse params that are primities correctly; use !r expr to use R expressions wihtin yaml
+  if(file.exists(filepath)) {
+    buf=c("params:")
+    for(p in 1:length(param.name)) buf=c(buf, paste0("\t",param.name[p], ": ",param.value[p]))
+    fcon <- file(filepath, 'r+')
+    l <- readLines(fcon)
+    yaml_block <- grepl(l,pattern="^---$")
+    idx = 2
+    if(any(yaml_block)) idx <- max(which(yaml_block))
+    params_block <- grepl(l,pattern="params:")
+    if(any(params_block)) {
+      idx <- max(which(params_block))
+      l <- c(l[1:idx],buf,l[idx+1:length(l)])#add inside yaml, after params: if exists
+    } else l <- c(l[1:(idx-1)],buf,l[idx:length(l)]) #add inside yaml, at end of yaml block
+    writeLines(l,fcon)
+    close(fcon)
+    return(TRUE)
+  } else return(FALSE)
 }
 
 reportUpdate <- function(reportName, outputDir=NULL) {
