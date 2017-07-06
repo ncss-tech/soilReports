9cc99ac2163a6bdf905b37de73c5a272495463d0
Brown <12001002844681@FEDIDCARD.GOV>
2017-07-06 10:37:59 -0700

    renamed README.md in RO2\mu-summary report setup.R manifest to match naming scheme of other RO2 reports

--- inst/reports/region2/mu-summary/setup.R
+++ inst/reports/region2/mu-summary/setup.R
@@ -8,8 +8,8 @@
 .report.name <- 'mu-summary'
 .report.version <- 1.0
 .report.description <- 'summarize raster data for a large collection of map unit polygons'
-.paths.to.copy <- c('report.Rmd','generate-samples.R','make-reports.R','config.R','README.md')
-.update.paths.to.copy <- c('report.Rmd','generate-samples.R','make-reports.R','README.md')
+.paths.to.copy <- c('report.Rmd','generate-samples.R','make-reports.R','config.R','NOTES.md')
+.update.paths.to.copy <- c('report.Rmd','generate-samples.R','make-reports.R','NOTES.md')
 .has.shiny.interface <- FALSE
 
 # packages + deps from CRAN
