1568932b0df1fe5d48148936b15a5e37402a27ec
Dylan Beaudette <dylan.beaudette@gmail.com>
2017-08-07 12:22:50 -0700

    this isn't perfect, but the default config. should have a sampling density of 1

--- inst/reports/region2/mu-comparison/config.R
+++ inst/reports/region2/mu-comparison/config.R
@@ -15,33 +15,29 @@
 
 raster.list <- list(
   continuous=list(
-    `Mean Annual Air Temperature (degrees C)`='L:/NRCS/MLRAShared/Geodata/climate/raster/final_MAAT_800m.tif', 
-    `Mean Annual Precipitation (mm)`='L:/NRCS/MLRAShared/Geodata/climate/raster/final_MAP_mm_800m.tif', 
-    `Effective Precipitation (mm)`='L:/NRCS/MLRAShared/Geodata/climate/raster/effective_precipitation_800m.tif',
-    `Frost-Free Days`='L:/NRCS/MLRAShared/Geodata/climate/raster/ffd_mean_800m.tif',
-    `Growing Degree Days (degrees C)`='L:/NRCS/MLRAShared/Geodata/climate/raster/gdd_mean_800m.tif',
-    `Elevation (m)`='L:/NRCS/MLRAShared/Geodata/elevation/10_meter/ca630_elev',
-    `Slope Gradient (%)`='L:/NRCS/MLRAShared/Geodata/elevation/10_meter/ca630_slope',
-    `Rain Fraction`='L:/NRCS/MLRAShared/Geodata/climate/raster/rain_fraction_mean_800m.tif',
-    `Annual Beam Radiance (MJ/sq.m)`='L:/NRCS/MLRAShared/Geodata/DEM_derived/beam_rad_sum_mj_30m.tif',
-    `(Estimated) MAST (degrees C)`='E:/geodata/ca630/soil_temperature/spatial_data/mast-model.tif',
-    `Compound Topographic Index`='L:/NRCS/MLRAShared/Geodata/DEM_derived/tci30.tif',
-    `SAGA TWI`='L:/NRCS/MLRAShared/Geodata/DEM_derived/saga_twi_10.tif',
-    `K40 percentage`='L:/NRCS/MLRAShared/Geodata/Radiometric/namrad_k_aea.tif'
+    `Mean Annual Air Temperature (degrees C)`='E:/gis_data/prism/final_MAAT_800m.tif',
+    `Mean Annual Precipitation (mm)`='E:/gis_data/prism/final_MAP_mm_800m.tif',
+    `Effective Precipitation (mm)`='E:/gis_data/prism/effective_precipitation_800m.tif',
+    `Frost-Free Days`='E:/gis_data/prism/ffd_mean_800m.tif',
+    `Growing Degree Days (degrees C)`='E:/gis_data/prism/gdd_mean_800m.tif',
+    `Elevation (m)`='E:/gis_data/region-2-mu-analysis/elev_30.tif',
+    `Slope Gradient (%)`='E:/gis_data/region-2-mu-analysis/slope_30.tif',
+    `Annual Beam Radiance (MJ/sq.m)`='E:/gis_data/ca630/beam_rad_sum_mj_30m.tif',
+    `(Estimated) MAST (degrees C)`='E:/gis_data/ca630/mast-model.tif',
+    `Compound Topographic Index`='E:/gis_data/ca630/tci30.tif',
+    `MRVBF`='E:/gis_data/ca630/mrvbf_10.tif',
+    `SAGA TWI`='E:/gis_data/ca630/saga_twi_10.tif'
   ),
   categorical=list(
-    `R1056ness`='E:/workspace/r1056ness_ras',
-    `Geomorphon Landforms`='L:/NRCS/MLRAShared/Geodata/DEM_derived/forms10.tif',
-    `Curvature Classes`='L:/NRCS/MLRAShared/Geodata/DEM_derived/curvature_classes_15.tif',
-    `NLCD`='L:/NRCS/MLRAShared/Geodata/NLCD/nlcd_ca630',
+    `Geomorphon Landforms`='E:/gis_data/region-2-mu-analysis/forms10_region2.tif',
+    `Curvature Classes`='E:/gis_data/region-2-mu-analysis/curvature_classes_10_class_region2.tif',
+    `NLCD 2011`='E:/gis_data/region-2-mu-analysis/nlcd_2011_cropped.tif',
     `Mesic Thermic Uncertainty`='S:/NRCS/Archive_Dylan_Beaudette/CA630-models/hobo_soil_temperature/spatial_data/mast-model-mesic_thermic-uncertainty.tif'
   ),
   circular=list(
-    `Slope Aspect (degrees)`='L:/NRCS/MLRAShared/Geodata/DEM_derived/ca630_aspect'
+    `Slope Aspect (degrees)`='E:/gis_data/region-2-mu-analysis/aspect_30.tif'
   )
 )
-
-
 
 ###################
 ### Map unit data #
@@ -61,7 +57,7 @@ raster.list <- list(
 # name of featureclass
 # mu.layer <- 'ca630_a'
 # map unit symbols / keys to extract
- mu.set <- c('6072','6071','6205','6034')
+ mu.set <- c('5012','5013','7011')
 
 
 
@@ -92,7 +88,7 @@ mu.col <- 'MUSYM'
 # increase if there are un-sampled polygons
 # delineations smaller than 5 ac. may require up to 5 points / ac.
 # values > 6-7 points / ac. will only slow things down
-pts.per.acre <- 0.01
+pts.per.acre <- 1
 
 
 
