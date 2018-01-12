4f6692ec0bf9bd034ef3a3ff3ffcaa26c72f08de
Dylan Beaudette <dylan.beaudette@gmail.com>
2018-01-12 11:09:20 -0800

    need the latest aqp until it gets pushed to CRAN

--- inst/reports/region2/mu-comparison/setup.R
+++ inst/reports/region2/mu-comparison/setup.R
@@ -19,7 +19,7 @@
 .packages.to.get <- c('knitr', 'rmarkdown', 'rgdal', 'reshape2', 'raster', 'plyr', 'Hmisc', 'aqp', 'soilDB', 'sharpshootR', 'latticeExtra', 'clhs', 'devtools', 'rgeos', 'randomForest', 'vegan', 'spdep', 'scales', 'e1071')
 
 ## packages from GH, no deps
-.gh.packages.to.get <- c('ncss-tech/sharpshootR')
+.gh.packages.to.get <- c('ncss-tech/sharpshootR', 'ncss-tech/aqp')
 
 # flag to indicate that shiny.Rmd and not report.Rmd is the primary markdown file; convention should probably be that shiny.Rmd has option to generate a static report.Rmd-type output. 
 # currently the pedon summary demo using Shiny supports this option and uses a file called report.Rmd as the template for the static output
