1d186dbe7006c406cf746a5c2f254f5a883488a7
Dylan Beaudette <dylan.beaudette@gmail.com>
2018-05-21 15:53:13 -0700

    there is currently nothing editable in the config file

--- inst/reports/region2/QA-summary/config.R
+++ inst/reports/region2/QA-summary/config.R
@@ -1,23 +1,6 @@
-### DMU Diff Report
-### 2018-03-12
+### QA Summary Report
+### 2018-05-21
 ### D.E. Beaudette
 ###
 ### configuration file, edit as needed
 ###
-
-## this is a work in progress
-## as of 2018-03-19 all setup is performed by establishing a reasonable SS in local NASIS
-# load project by name
-# add data for new MLRA MU via project mapunit -> mapunit table
-# add data for new MLRA DMU via mapunit/correlation -> data mapunit table
-
-
-
-###################################
-### project information           #
-###################################
-
-# for now, used in the report title
-# define project name
-project.name <- 'MLRA 19/14 - Perkins gravelly loam, 2 to 5 percent slopes'
-
