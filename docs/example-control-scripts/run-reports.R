9553fc903e9242e15b9280b29e1ea533c6b44d53
Dylan Beaudette <dylan.beaudette@gmail.com>
2018-05-22 14:55:33 -0700

    fixes and some figures for documentation

--- docs/example-control-scripts/run-reports.R
+++ docs/example-control-scripts/run-reports.R
@@ -1,24 +1,30 @@
 library(rmarkdown)
 
 # project name
-p <- 'MLRA 17 - Arbuckle horizon standardization in 2-CHI SSA updated in SDJR'
+p <- 'MLRA 17/14 - Sycamore silty clay loam'
 
 # output directory
-d <- '../2-CHI/FY2018/Arbuckle/'
+d <- '../2-CHI/FY2018/Sycamore/Sycamore silty clay loam/'
 
-
+## notes:
 # reports are stored in the same parent folder as this script
+# envir = new.env() ensures that the current environment isn't polluted with objects used in reports
+
 render(input = 'DMU-diff/report.Rmd',
        output_dir = d, 
        output_file = 'DMU-diff.html',
        params = list(projectname=p), 
-       clean = TRUE
+       clean = TRUE,
+       envir = new.env()
       )
+
+
 
 render(input = 'QA-summary/report.Rmd',
        output_dir = d, 
        output_file = 'QA-summary.html',
        params = list(projectname=p), runtime = 'static', 
-       clean = TRUE
+       clean = TRUE,
+       envir = new.env()
 )
 
