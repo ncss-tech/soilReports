60bb36ecbfa04f4cec99d8c18dacba21c41b8c9d
Dylan Beaudette <dylan.beaudette@gmail.com>
2018-06-01 16:06:33 -0700

    one more example

--- docs/SDA-example/get-mu-from-SDA.R
+++ docs/SDA-example/get-mu-from-SDA.R
@@ -10,6 +10,13 @@ FROM mapunit
 CROSS APPLY SDA_Get_MupolygonWktWgs84_from_Mukey(mapunit.mukey) as G
 WHERE mukey IN (SELECT DISTINCT mukey FROM component WHERE compname like 'amador%' AND majcompflag = 'Yes')"
 
+# get polygons associated with specific mukey
+q <- "select G.MupolygonWktWgs84 as geom, mapunit.mukey, muname
+FROM mapunit
+CROSS APPLY SDA_Get_MupolygonWktWgs84_from_Mukey(mapunit.mukey) as G
+WHERE mukey IN ('458818', '461942', '462112', '463133')"
+
+
 # result is a data.frame, "MupolygonWktWgs84" contains WKT representation of geometry
 res <- SDA_query(q)
 
@@ -20,4 +27,4 @@ s <- processSDA_WKT(res)
 s <- spTransform(s, CRS('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,-0,-0,-0,0 +units=m +no_defs'))
 
 # save in current folder as SHP
-writeOGR(s, dsn='.', layer='amador', driver='ESRI Shapefile')
+writeOGR(s, dsn='.', layer='redding', driver='ESRI Shapefile')
