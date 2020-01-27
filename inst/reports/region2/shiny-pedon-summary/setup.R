090a90fd80b7c5b90fa5d6cdc508ef563d6a4edf
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2020-01-27 09:30:43 -0800

    update shiny pedon setup.R

--- inst/reports/region2/shiny-pedon-summary/setup.R
+++ inst/reports/region2/shiny-pedon-summary/setup.R
@@ -5,15 +5,21 @@
 
 ## note: this will not update installed packages... could lead to dependency-related errors
 ## packages + deps from CRAN
-.packages.to.get <- c('soilDB','sharpshootR','rgdal','raster','leaflet','mapview','latticeExtra','rmarkdown','plyr','reshape2','xtable','knitr','shiny','flexdashboard')
+.packages.to.get <- c('soilDB','sharpshootR','rgdal',
+                      'raster','leaflet','mapview',
+                      'latticeExtra','rmarkdown','plyr',
+                      'reshape2','xtable','knitr',
+                      'shiny','flexdashboard')
 
 
 ## packages from GH, no deps
-.gh.packages.to.get <- c('ncss-tech/soilDB','ncss-tech/sharpshootR')
+.gh.packages.to.get <- c('ncss-tech/aqp','ncss-tech/soilDB','ncss-tech/sharpshootR')
 
 .report.name <- 'shiny-pedon-summary'
-.report.version <- '0.2'
-.report.description <- 'interactively subset and summarize NASIS pedon data from one or more map units'
+.report.version <- '1.0'
+.report.description <- 'Interactively subset and summarize NASIS pedon data from one or more map units'
 
-.paths.to.copy <- c('report.Rmd','shiny.Rmd','utility_functions.R','main.R','config.R','changes.txt')
-.update.paths.to.copy <- c('report.Rmd','shiny.Rmd','utility_functions.R','main.R')
+.paths.to.copy <- c('report.Rmd','shiny.Rmd','util.R','config.R','changes.txt')
+.update.paths.to.copy <- c('report.Rmd','shiny.Rmd','util.R')
+
+.has.shiny.interface <- TRUE
