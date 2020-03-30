3c028160d5324e29acf109fa6616bd0258d8d78d
Jay Skovlin <12001000116602@FEDIDCARD.GOV>
2020-03-30 15:06:35 -0600

    Initial commit for PLSS locator shiny app report
    Still needs tweaking to smooth out functionality - ideas and help
    welcome!

---
+++ inst/reports/region4/shiny-plss-locator/config.R
@@ -0,0 +1,35 @@
+#load packages
+library(shiny)
+library(DT)
+library(ggplot2)
+library(sharpshootR)
+library(stringi)
+library(sp)
+library(raster)
+library(rgeos)
+library(httr)
+library(RgoogleMaps)
+library(leaflet)
+library(leaflet.extras)
+library(jpeg)
+library(mapview)
+library(leafsync)
+library(leafem)
+library(curl)
+library(elevatr)
+library(cluster)
+
+# set local system path for output csv file
+path <- 'C:\\PLSS_test\\'
+
+# defaults for startup
+input <- list()
+input$m <- "MT20"
+# input$s <- "07"
+# input$t <- "14N"
+# input$r <- "11W"
+
+#TODO: other settings??
+
+
+
