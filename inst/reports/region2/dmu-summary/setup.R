d04b01c32c5ba4cbf18b2a11622432e229f25001
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-03-22 11:52:51 -0700

    Add setup.R to region2/dmu-summary

---
+++ inst/reports/region2/dmu-summary/setup.R
@@ -0,0 +1,28 @@
+##
+## Internal setup file for report -- 
+##   change values as needed but ensure all `.variable.names` are defined for each report
+##
+
+## packages from CRAN
+.packages.to.get <- c('aqp','soilDB','sharpshootR','latticeExtra','reshape2','tactile','kableExtra')
+
+## packages from GitHub, installed with no dependencies
+.gh.packages.to.get <- c('ncss-tech/aqp','ncss-tech/soilDB','ncss-tech/sharpshootR')
+
+# name of report (matches parent folder name; e.g. `inst/reports/templates/minimal`)
+.report.name <- 'dmu-summary'
+
+# version of report
+.report.version <- 'beta'
+
+# brief description for `soilReports::listReports()`
+.report.description <- 'DMU Summary Report'
+
+# these are the files to copy on initial installation with copyReport/reportInit
+.paths.to.copy <- c('report.Rmd', 'custom.R', 'cache-data.R')
+
+# these are the files to copy on reportUpdate
+.update.paths.to.copy <- c('report.Rmd', 'custom.R', 'cache-data.R')
+
+# this is a flag to denote whether `shiny.Rmd` is main entrypoint to report
+.has.shiny.interface <- FALSE
