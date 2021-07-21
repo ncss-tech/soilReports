91586cd2fba30f7ad6c81b216c5cb0af9d8fb939
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-07-21 08:32:35 -0700

    Add gdb-acreage report

---
+++ inst/reports/region2/gdb-acreage/NEWS.md
@@ -0,0 +1,2 @@
+# gdb-acreage 1.0 (2021-07-21)
+- initial commit to soilReports package
---
+++ inst/reports/region2/gdb-acreage/config.R
@@ -0,0 +1,7 @@
+# config.R 
+
+poly.dsn <- "E:/CA649/Geodata/GDB/FGCA649_Projects_2021_07_16.gdb"
+poly.layer <- "ca649_a"
+poly.bounds <- "ca649_b"
+area.symbol <- "CA649"
+order.by.col <- "Change" # options: Change, MUSYM, Legend_Acres, Spatial_Acres, Status, MURecID, Match
