f1c633724e917c2f0a3a25bfce263957e21663d4
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-01-20 13:37:06 -0800

    for #116

--- R/reportSetup.R
+++ R/reportSetup.R
@@ -27,49 +27,53 @@
 #' Install packages needed for a report
 #'
 #' @param reportName Name of report, as found in `listReports.` Format: `directory/reportName`.
-#' @param upgrade Upgrade CRAN packages? Default: \code{FALSE}
+#' @param upgrade Upgrade CRAN packages? Default: `TRUE`
 #'
 #' @return Installed packages from CRAN and GitHub in user library, as specified in report-specific manifest.
 #' 
 #' @export
 #'
-reportSetup <- function(reportName, upgrade=FALSE) {
+reportSetup <- function(reportName, upgrade = TRUE) {
   
   # get base directory where reports are stored within package
-  base.dir <- system.file(paste0('reports/', reportName), package='soilReports')
+  base.dir <- system.file(paste0('reports/', reportName), package = 'soilReports')
   
-  if(dir.exists(base.dir)) {
+  if (dir.exists(base.dir)) {
     # all reports must have setup.R file
     setup.file <- paste0(base.dir, '/setup.R')
     
-    if(file.exists(setup.file)) {
+    if (file.exists(setup.file)) {
       # source file into temp environment
       env <- new.env()
       sys.source(setup.file, envir = env)
       
       # install any missing packages from CRAN
-      if(exists('.packages.to.get', envir = env)) {
-      p <- get('.packages.to.get', envir = env)
-        .ipkCRAN(p, up=upgrade)
-      } else message("no CRAN packages listed as dependencies")
+      if (exists('.packages.to.get', envir = env)) {
+        p <- get('.packages.to.get', envir = env)
+        .ipkCRAN(p, up = upgrade)
+      } else
+        message("no CRAN packages listed as dependencies")
       
       # install any missing packages from GH
-      if(exists('.gh.packages.to.get', envir = env)) {
+      if (exists('.gh.packages.to.get', envir = env)) {
         p <- get('.gh.packages.to.get', envir = env)
         .ipkGH(p)
-      } else message("no GitHub packages listed as dependencies")
-     
+      } else
+        message("no GitHub packages listed as dependencies")
+      
       # perform any manual fixes specified in the setup.R
       # this is a list, each item is a command
-      if(exists('.fixes', envir = env)) {
+      if (exists('.fixes', envir = env)) {
         message('applying manual fixes...')
         f <- get('.fixes', envir = env)
-        sapply(f, function(i) eval(parse(text=i)))
-      } 
-        
+        sapply(f, function(i)
+          eval(parse(text = i)))
+      }
+      
       # let user know that we are ready to go
-      message('required packages are installed!') 
-    } else stop(sprintf("setup.R does not exist in %s", base.dir), call.=FALSE)
-  } else stop(sprintf("report %s does not exist", base.dir), call.=FALSE)
+      message('required packages are installed!')
+    } else
+      stop(sprintf("setup.R does not exist in %s", base.dir), call. = FALSE)
+  } else
+    stop(sprintf("report %s does not exist", base.dir), call. = FALSE)
 }
-
