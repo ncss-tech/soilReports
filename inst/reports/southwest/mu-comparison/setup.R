e73b3d0bdf1379679a91c5b74a0d3a1dcd246932
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-04-03 13:12:59 -0700

    cleanup old messages

--- inst/reports/southwest/mu-comparison/setup.R
+++ inst/reports/southwest/mu-comparison/setup.R
@@ -67,9 +67,3 @@
 # flag to indicate that shiny.Rmd and not report.Rmd is the primary markdown file; convention should probably be that shiny.Rmd has option to generate a static report.Rmd-type output. 
 # currently the pedon summary demo using Shiny supports this option and uses a file called report.Rmd as the template for the static output
 .has.shiny.interface <- FALSE
-
-## fixes, usually due to an older version of R
-
-# get an older version of knitr,  work-around, until we get R 3.3.0
-# http://stackoverflow.com/questions/37241578/getting-a-parser-all-error-in-r-when-using-knitr-for-converting-a-basic-rmd-file
-# .fixes <- list('install.packages("http://cran.r-project.org/src/contrib/Archive/knitr/knitr_1.12.tar.gz", repos=NULL, type="source")')
